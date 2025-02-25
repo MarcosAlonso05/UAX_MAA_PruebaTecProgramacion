40ff16dd47b280e374eb6d0dd221d2a71571328e
Marcos Alonso <marcosalonsoamor05@gmail.com>
2025-02-26 00:56:36 +0100

    Programa 2 hecho

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import programa_1.aplicacion.Principal;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Ejecutar programa 1:");
+
         String[] argumentos = {"200000"};
-        Principal.main(argumentos);
+        programa_1.aplicacion.Principal.main(argumentos);
+
+        System.out.println("Ejecutar programa 2:");
+        programa_2.aplicacion.Principal.main(new String[0]);
     }
 }
